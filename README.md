42c3cfbb3fe6a0b88da1f4230d93319252da0799
favoured mwange <mwangefavoured@gmail.com>
2024-07-22 10:10:51 +0300

    phase-1 project

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Phase-1-project
+# My Weather App
+This application  allows users to view in detail the current weather conditions of various cities on the globe. Users are able to evaluate what type of clothing to wear or whether to continue with their scheduled plans.
+
+## Features
+1. Allows users to enter the city name
+2. Fetches data about the weather experienced by the city from an API
+3. Displays the fetched data to the user
+4. Allows the user to give feedback on their user experience
+
+## Instructions
+
+# Installation
+ 1. Clone the repository to your PC
+ 2. Open the index.html file on your browser
+
+### View Weather Details
+When the page loads the first item that will be on view is a blank input option and a search button that performs the submit command
+
+### View Weather Condition (Loading The Weather App)
+In order for one to view the weather condition:
+   1. Inside the empty input option with the placeholder (ENTER CITY), input the name of the city you want
+   2. Once done with the input, hover your pointer to the search button (⌕) click on it.
+
+Upon doing the above the weather conditions for the specified city will load.   
+
+### Weather Menu
+Once the content loads up the user will be able to view the following;
+   1. An image showing the current weather condition being experienced by the city:
+        We determined the image to be viewed using the criteria "data.weather[0].id" as provided by the Openweather API we were using.
+   2. An arithmetic value at the center indicating the current temperature of the city in degrees celcious
+   3. The name of the city 
+   4. An arithmetic value in percentage indicating the amount of water present in the atmosphere of the 
+      city  (Humidity)
+   5. An arithmetic value in kilometres per hour indicating the speed of wind experienced by the city.
+
+   * The details above do not persist upon refreshing the page
---
+++ index.js
@@ -0,0 +1,64 @@
+// Creating a variable for the API KEY and URL
+const apiKey= "4272e49c4a4cc211baf48675dc8d1cfe";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+// require('dotenv').config()
+// const apiKey=process.env.API_KEY
+
+// Selecting Elements for manipulating the DOM
+
+const searchBox = document.querySelector("#search input")
+const searchBtn = document.querySelector("#search button")
+const condition = document.querySelector(".condition")
+const comments = document.getElementById('comments')
+const commentBtn = document.getElementById('btn')
+
+// Getting the data from the API
+
+ function checkWeather(city){
+    fetch(apiUrl + city + `&appid=${apiKey}`)
+    .then(response=>response.json())
+    .then(data=>{
+
+    // Adding the fetched data to the DOM
+
+    document.querySelector(".city").textContent= data.name;
+    document.querySelector(".temp").textContent= data.main.temp + "°C";
+    document.querySelector(".humidity").textContent= data.main.humidity + "%";
+    document.querySelector(".wind").textContent= data.wind.speed + "km/h";
+
+    // Displaying the weather condition being experienced using images
+
+   switch (true){
+    case(data.weather[0].id >=200 && data.weather[0].id <300):
+    return condition.src= "/images/rain.png";
+    case(data.weather[0].id >=300 && data.weather[0].id <400):
+    return condition.src= "/images/rain.png";
+    case(data.weather[0].id >=500 && data.weather[0].id <600):
+    return condition.src= "/images/rain.png";
+    case(data.weather[0].id >=600 && data.weather[0].id <700):
+    return condition.src= "/images/snow.png";
+    case(data.weather[0].id >=700 && data.weather[0].id <800):
+    return condition.src= "/images/mist.png";
+    case(data.weather[0].id === 800):
+    return condition.src= "/images/clear.png";
+    case(data.weather[0].id >=801 && data.weather[0].id <805):
+    return  condition.src= "/images/clouds.png";
+    default:
+     return condition.src="/images/unknown.webp"   
+   }
+})
+// Displaying the weather condition
+document.querySelector(".weather").style.display="block"
+}
+
+// Adding Event listeners 
+searchBtn.addEventListener("click", (event)=>{
+    event.preventDefault()
+    checkWeather(searchBox.value)
+})
+
+commentBtn.addEventListener("click",(event)=>{
+event.preventDefault();
+comments.value
+alert('SUBMITTED SUCCESFULLY! Thank you for your feedback')
+})
